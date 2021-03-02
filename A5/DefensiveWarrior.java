25df3c76dfb458a0f3caca8541b4c08af46eae0a
Kunal Kak <kunalrkak@gmail.com>
2021-03-01 20:23:56 -0500

    init

--- A5/DefensiveWarrior.java
+++ A5/DefensiveWarrior.java
@@ -1,97 +1,28 @@
 
 public class DefensiveWarrior extends Warrior {
 
-	private int level;
-	private int attack;
-	private int defense;
+	private static final int DEFAULT_ATTACK = 2;
+	private static final int DEFAULT_DEFENSE = 3;
 
-	private DefensiveWarrior(int level) {
-
-		this.level = level;
-
+	public DefensiveWarrior(int level, int attack, int defense) {
+		super(level, attack, defense);
 	}
 
-	@Override
-	public int getLevel() {
-
-		return level;
-
+	public static Builder builder() {
+		return new Builder();
 	}
 
-	@Override
-	public int getAttack() {
+	public static class Builder extends Warrior.Builder<DefensiveWarrior>{
 
-		return attack;
-
-	}
-
-	@Override
-	public int getDefense() {
-
-		return defense;
-
-	}
-
-	public static class Builder {
-
-		private DefensiveWarrior defensiveWarrior;
-
-		public Builder(int level) {
-
-			defensiveWarrior = new DefensiveWarrior(level);
-			defensiveWarrior.attack = 2;
-			defensiveWarrior.defense = 3;
-
+		public Builder() {
+			super(DEFAULT_ATTACK, DEFAULT_DEFENSE);
 		}
 
-		public Builder attack(int attack) {
-
-			defensiveWarrior.attack = attack;
-			return this;
-
-		}
-
-		public Builder defense(int defense) {
-
-			defensiveWarrior.defense = defense;
-			return this;
-
-		}
-
+		@Override
 		public DefensiveWarrior build() {
-
-			validate(defensiveWarrior);
-			return defensiveWarrior;
-
+			validate();
+			return new DefensiveWarrior(super.level, super.attack, super.defense);
 		}
-
-		private void validate(DefensiveWarrior defensiveWarrior) {
-
-			boolean errorOccurred = false;
-			String levelErrorMessage = "", attackErrorMessage = "", defenseErrorMessage = "";
-
-			if (defensiveWarrior.level < 0) {
-				levelErrorMessage = "Level must be greater than 0. ";
-				errorOccurred = true;
-			}
-
-			if (defensiveWarrior.attack < 0) {
-				attackErrorMessage = "Attack must be greater than 0. ";
-				errorOccurred = true;
-			}
-
-			if (defensiveWarrior.defense < 0) {
-				defenseErrorMessage = "Defense must be greater than 0. ";
-				errorOccurred = true;
-			}
-
-			if (errorOccurred) {
-
-				throw new IllegalStateException(levelErrorMessage + attackErrorMessage + defenseErrorMessage);
-
-			}
-		}
-
 	}
 
 }
--- A5/Warrior.java
+++ A5/Warrior.java
@@ -1,10 +1,79 @@
 
 public abstract class Warrior {
+	private int level;
+	private int attack;
+	private int defense;
 
-	public abstract int getLevel();
+	public int getLevel() {
+		return level;
+	}
 
-	public abstract int getAttack();
+	public int getAttack() {
+		return attack;
+	}
 
-	public abstract int getDefense();
+	public int getDefense() {
+		return defense;
+	}
+
+	public static abstract class Builder<T> {
+		int level;
+		int attack;
+		int defense;
+
+		private boolean _isLevelInitialized = false;
+
+		public Builder(int defaultAttack, int defaultDefense) {
+			this.attack = defaultAttack;
+			this.defense = defaultDefense;
+		}
+
+		public Builder level(int level) {
+			this.level = level;
+			return this;
+		}
+
+		public Builder attack(int attack) {
+			this.attack = attack;
+			return this;
+		}
+
+		public Builder defense(int defense) {
+			this.defense = defense;
+			return this;
+		}
+
+		public abstract T build();
+
+		void validate() {
+			boolean errorOccurred = false;
+			String levelErrorMessage = "", attackErrorMessage = "", defenseErrorMessage = "";
+
+			if (!this._isLevelInitialized) {
+				levelErrorMessage = "Level is a required field. ";
+				errorOccurred = true;
+			}
+
+			if (defensiveWarrior.level < 0) {
+				levelErrorMessage = "Level must be greater than 0. ";
+				errorOccurred = true;
+			}
+
+			if (defensiveWarrior.attack < 0) {
+				attackErrorMessage = "Attack must be greater than 0. ";
+				errorOccurred = true;
+			}
+
+			if (defensiveWarrior.defense < 0) {
+				defenseErrorMessage = "Defense must be greater than 0. ";
+				errorOccurred = true;
+			}
+
+			if (errorOccurred) {
+				throw new IllegalStateException(levelErrorMessage + attackErrorMessage + defenseErrorMessage);
+			}
+		}
+
+	}
 
 }
